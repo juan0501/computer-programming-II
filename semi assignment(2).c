0fa642c43e18c0143556c190e68a903c10bb9708
Ah-yeong <84187163+Ah-yeong@users.noreply.github.com>
2021-07-06 00:19:30 +0900

    Add files via upload

---
+++ semi assignment(2).c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    int i, j, k, l;
+
+    printf("มู(line) ผ๖: ");
+    scanf("%d", &num);
+
+    for (i = 0; i < num; i++)
+    {
+        for (j = 0; j < i; j++)
+            printf(" ");
+
+        for (k = 1; k < num-i; k++)
+            printf("%d", k);
+
+        for (l = k; l < k+num-i; l++)
+            printf("%d", l);
+
+        printf("\n");
+    }
+
+    return 0;
+}
